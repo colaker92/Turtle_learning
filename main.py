e205bb72b7ab85f7170c11c5a3cdcfae39195537
Emre Çolaker <2estore0902@gmail.com>
2025-12-17 23:15:31 +0300

    Polygon Math Added

--- main.py
+++ main.py
@@ -56,11 +56,27 @@ turtle_instance.left(36)
 
 turtle.done()
 '''
-
+''' 
 # loop ile yapımı test
 turtle_instance = turtle.Turtle()
 for i in range(5):
     turtle_instance.right(144)
     turtle_instance.forward(100)
 turtle.done()
+'''
+
+# polygon yapımı
+
+# matematik formülü ile çizim
+
+turtle_instance = turtle.Turtle()
+num_sides = 5 # kaç kenarlı polygon çizeceksek o değeri atıyoruz
+angle = 360.0 / num_sides # 360.0 -> derece ifadesi / num_sides -> kenar sayısı
+side_lenght = 100 # ne kadar ileri gideceği
+
+for i in range(num_sides):
+    turtle_instance.right(angle)
+    turtle_instance.forward(side_lenght)
+turtle.done()
+
 
